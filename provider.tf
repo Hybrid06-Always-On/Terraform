66efbeacbd2ec4b3739836b1c4afc10c2bb269cf
leeeeejieun <arjleun19@gmail.com>
2026-02-03 17:08:09 +0900

    Feat. Provider 설정

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.28.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2" # 서울 리전
+}
+
