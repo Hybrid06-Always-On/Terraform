496c2038667208a5e4952e44b9922a7a483908d9
leeeeejieun <arjleun19@gmail.com>
2026-02-06 14:30:55 +0900

    Feat. network 모듈 백엔드 설정 추가

---
+++ modules/network/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket       = "team-tfstate-bucket"
+    key          = "module/network/terraform.tfstate"
+    region       = "ap-northeast-2"
+    use_lockfile = true # lock 파일 저장
+    encrypt      = true # 암호화 저장
+    profile      = "process"
+  }
+}
