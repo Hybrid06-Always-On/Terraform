9f6f9e9594220c3bb78b6a8972ee433e0b5b8e07
DONGHYEOK137 <DONGHYEOK137>
2026-02-13 19:09:38 +0900

    커밋 메시지 입력

--- modules/route53/route53-failover.tf
+++ modules/route53/route53-failover.tf
@@ -2,7 +2,7 @@
 # 1️⃣ 온프레미스 Health Check
 ############################################
 resource "aws_route53_health_check" "onprem" {
-  ip_address        = "너의_공인_IP"
+  ip_address        = "121.160.41.59"
   port              = 80
   type              = "HTTP"
   resource_path     = "/"
@@ -19,10 +19,12 @@ resource "aws_route53_record" "primary_root" {
   type    = "A"
 
   set_identifier = "primary-onprem"
-  failover       = "PRIMARY"
+  failover_routing_policy {
+    type = "PRIMARY"
+  }
 
   ttl     = 60
-  records = ["너의_공인_IP"]
+  records = ["121.160.41.59"]
 
   health_check_id = aws_route53_health_check.onprem.id
 }
@@ -33,10 +35,12 @@ resource "aws_route53_record" "primary_www" {
   type    = "A"
 
   set_identifier = "primary-onprem-www"
-  failover       = "PRIMARY"
+  failover_routing_policy {
+    type = "PRIMARY"
+  }
 
   ttl     = 60
-  records = ["너의_공인_IP"]
+  records = ["121.160.41.59"]
 
   health_check_id = aws_route53_health_check.onprem.id
 }
@@ -50,7 +54,9 @@ resource "aws_route53_record" "secondary_root" {
   type    = "A"
 
   set_identifier = "secondary-cloud"
-  failover       = "SECONDARY"
+  failover_routing_policy {
+    type = "SECONDARY"
+  }
 
   alias {
     name                   = aws_cloudfront_distribution.this.domain_name
@@ -65,7 +71,9 @@ resource "aws_route53_record" "secondary_www" {
   type    = "A"
 
   set_identifier = "secondary-cloud-www"
-  failover       = "SECONDARY"
+  failover_routing_policy {
+    type = "SECONDARY"
+  }
 
   alias {
     name                   = aws_cloudfront_distribution.this.domain_name
